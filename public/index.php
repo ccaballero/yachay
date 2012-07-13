8487949edb7892e8a96a50274d294a6c2aab61f3
Carlos Caballero <cijkb.j@gmail.com>
2012-07-13 15:22:39 -0400

    [LAYOUT] Restructuring the layout of files

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ mb_http_output('UTF-8');
 
 // Define path to application directory
 defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/..'));
 
 // Define application environment
 defined('APPLICATION_ENV')
@@ -13,18 +13,32 @@ defined('APPLICATION_ENV')
 
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
-    realpath(APPLICATION_PATH . '/../library'),
+    realpath(APPLICATION_PATH . '/library'),
     get_include_path(),
 )));
+
+/* Zend_Config */
+require_once 'Zend/Config/Ini.php';
+
+$config = new Zend_Config_Ini(
+    APPLICATION_PATH . '/configs/application.ini',
+    APPLICATION_ENV,
+    array('allowModifications' => true)
+);
+
+$local = new Zend_Config_Ini(
+    APPLICATION_PATH . '/configs/local.ini',
+    APPLICATION_ENV
+);
+
+$config->merge($local);
+$config->setReadOnly();
 
 /** Zend_Application */
 require_once 'Zend/Application.php';
 
 // Create application, bootstrap, and run
-$application = new Zend_Application(
-    APPLICATION_ENV,
-    APPLICATION_PATH . '/configs/application.ini'
-);
+$application = new Zend_Application(APPLICATION_ENV, $config);
 
 $application->bootstrap()
             ->run();
